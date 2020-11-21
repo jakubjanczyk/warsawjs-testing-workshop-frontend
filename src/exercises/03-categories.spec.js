0dc7fde599d159af81e163bc08309dfde5ce9663
Jakub Janczyk <kubex321@gmail.com>
2020-11-21 16:06:18 +0100

    demo: 03-categories

--- src/exercises/03-categories.spec.js
+++ src/exercises/03-categories.spec.js
@@ -5,10 +5,23 @@ import { Categories } from '../products-list/Categories/Categories';
 
 describe('Categories', () => {
     it('should display provided categories', () => {
-        // DEMO
+        const categories = [{key: 'smartfon', label: 'smartfon'}, {key: 'smartwatch', label: 'smartwatch'}];
+        render(<Categories categories={categories} />);
+
+        const items = screen.getAllByRole('listitem');
+
+        expect(items.map(item => item.textContent)).toEqual(['smartfon', 'smartwatch']);
     });
 
     it('should allow to select provided category', () => {
-        // DEMO
+        const categories = [{key: 'smartfon', label: 'smartfon'}, {key: 'smartwatch', label: 'smartwatch'}];
+        const handleSelect = jest.fn();
+        render(<Categories categories={categories} onSelect={handleSelect} />);
+
+        const item = screen.getByText('smartfon');
+        userEvent.click(item);
+
+        expect(handleSelect).toHaveBeenCalledWith('smartfon');
+        expect(screen.getByTestId('selected-category')).toEqual(item);
     });
 });

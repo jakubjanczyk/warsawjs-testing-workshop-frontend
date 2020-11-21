4d823a53267a19da359d799dfde4d7afd4704bfa
Jakub Janczyk <kubex321@gmail.com>
2020-11-21 16:38:13 +0100

    demo: 05-paging

--- src/exercises/05-products-list-paging.spec.js
+++ src/exercises/05-products-list-paging.spec.js
@@ -1,15 +1,30 @@
 import { cleanup, render, screen, waitFor, waitForElementToBeRemoved, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { ProductsList } from '../products-list/ProductsList';
 import nock from 'nock';
-import { MemoryRouter } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { getDisplayedProducts, renderComponent } from './utils/products-list-utils';
 
 describe('ProductsList Paging', () => {
+    beforeEach(() => {
+        nock.cleanAll();
+    });
+
+    afterEach(cleanup);
 
     it('should show maximum 5 products by default', async () => {
-        // DEMO
+        const products = [
+            { id: '2', name: 'Apple', shortDescription: '' },
+            { id: '5', name: 'Apple 10', shortDescription: '' },
+            { id: '4', name: 'Apple 11', shortDescription: '' },
+            { id: '3', name: 'Apple 12', shortDescription: '' },
+            { id: '6', name: 'Apple 9', shortDescription: '' },
+            { id: '1', name: 'Samsung', shortDescription: '' }
+        ];
+        await renderComponent(products);
+
+        const productsNames = await getDisplayedProducts();
+
+        expect(productsNames).toEqual(['Apple', 'Apple 10', 'Apple 11', 'Apple 12', 'Apple 9']);
     });
 
 

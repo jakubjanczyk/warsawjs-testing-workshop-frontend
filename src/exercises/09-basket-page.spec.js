1b43429aa6f3202a253c563a20da30b285979f22
Jakub Janczyk <kubex321@gmail.com>
2020-11-21 17:03:48 +0100

    finished: 09-basket

--- src/exercises/09-basket-page.spec.js
+++ src/exercises/09-basket-page.spec.js
@@ -26,6 +26,8 @@ describe('Basket Page', () => {
         );
     };
 
+    const getDisplayedBasketItems = () => screen.getAllByTestId('product-name').map(item => item.textContent);
+
     it('should display products from basket', async () => {
         const initialState = {
             basket: [
@@ -35,23 +37,53 @@ describe('Basket Page', () => {
         }
         renderComponent(initialState);
 
-        const basketItems = screen.getAllByTestId('product-name');
-
-        expect(basketItems.map(item => item.textContent)).toEqual(['Samsung', 'Apple']);
+        expect(getDisplayedBasketItems()).toEqual(['Samsung', 'Apple']);
     });
 
     // TODO: Napisz test, który sprawdzi, że wyświetlona została poprawna suma cen produktów
     it('should display total price for all items', async () => {
+        const initialState = {
+            basket: [
+                {id: '1', name: 'Samsung', count: 1, remaining: 10, price: 1200.0},
+                {id: '2', name: 'Apple', count: 2, remaining: 10, price: 1500.0}
+            ]
+        }
+        renderComponent(initialState);
 
+        expect(screen.getByText(/Suma:/)).toHaveTextContent(/4200\.00/);
     });
 
     // TODO: Napisz test, który usunie jeden z produktów z koszyka
     it('should remove product from basket', async () => {
+        const initialState = {
+            basket: [
+                {id: '1', name: 'Samsung', count: 1, remaining: 10, price: 1200.0},
+                {id: '2', name: 'Apple', count: 2, remaining: 10, price: 1500.0}
+            ]
+        }
+        renderComponent(initialState);
 
+        const basketItemToRemove = screen.getByText(/Samsung/i, {selector: 'strong'}).closest('li');
+        const removeButton = within(basketItemToRemove).getByRole('button', {name: /Usuń/i});
+        userEvent.click(removeButton);
+
+        expect(getDisplayedBasketItems()).toEqual(['Apple']);
     });
 
     // TODO: Napisz test, który zmieni ilość jednego z produktów
     it('should allow to add amount to one of the product and change price', async () => {
+        const initialState = {
+            basket: [
+                {id: '1', name: 'Samsung', count: 1, remaining: 10, price: 1200.0},
+                {id: '2', name: 'Apple', count: 2, remaining: 10, price: 1500.0}
+            ]
+        }
+        renderComponent(initialState);
 
+        const basketItem = screen.getByText(/Samsung/i, {selector: 'strong'}).closest('li');
+        const increaseButton = within(basketItem).getByRole('button', {name: /\+/});
+        userEvent.click(increaseButton);
+
+        expect(screen.getByText(/Suma:/)).toHaveTextContent(/5400\.00/);
     });
 });

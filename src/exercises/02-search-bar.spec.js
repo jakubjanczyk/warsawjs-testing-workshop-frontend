21abd0681a0ac65af03c043975af482740199b62
Jakub Janczyk <kubex321@gmail.com>
2020-11-21 15:49:00 +0100

    demo: 02-search-bar

--- src/exercises/02-search-bar.spec.js
+++ src/exercises/02-search-bar.spec.js
@@ -5,16 +5,39 @@ import React from 'react';
 
 describe('SearchBar', () => {
 
+    const getInputAndTypeText = text => {
+        const input = screen.getByPlaceholderText('Szukaj produktu...');
+        userEvent.type(input, text);
+        return input;
+    };
+
+    const getClearButton = () => {
+        return screen.queryByRole('button', { name: /Wyczyść/ });
+    };
+
     it('should show typed text in input', () => {
-        // DEMO
+        render(<SearchBar />);
+
+        const input = getInputAndTypeText('samsung');
+
+        expect(input).toHaveDisplayValue('samsung');
     });
 
     it('should not show clear button when no text typed', () => {
-        // DEMO
+        render(<SearchBar />);
+
+        const clearButton = getClearButton();
+
+        expect(clearButton).not.toBeInTheDocument();
     });
 
     it('should show clear button when some text typed', () => {
-        // DEMO
+        render(<SearchBar />);
+
+        getInputAndTypeText('samsung');
+        const clearButton = getClearButton();
+
+        expect(clearButton).toBeInTheDocument();
     });
 
     // TODO Zadania:

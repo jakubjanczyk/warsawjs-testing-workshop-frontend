51adf38b690b58c72e426c9b816b3b3cdae02fbf
Jakub Janczyk <kubex321@gmail.com>
2020-11-21 16:49:06 +0100

    finished: 07-routing

--- src/exercises/07-products-list-routing.spec.js
+++ src/exercises/07-products-list-routing.spec.js
@@ -1,13 +1,24 @@
-import { cleanup, render, screen, waitFor, waitForElementToBeRemoved, within } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { ProductsList } from '../products-list/ProductsList';
-import nock from 'nock';
-import { MemoryRouter } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { givenCategories, givenProducts } from './utils/products-list-utils';
 
 describe('ProductsList Routing', () => {
     it('should go to product page when clicked on product name', async () => {
-        // DEMO
+        const products = [
+            { id: '2', name: 'Apple', shortDescription: '' },
+            { id: '1', name: 'Samsung', shortDescription: '' }
+        ];
+        givenProducts(products);
+        givenCategories([]);
+        render(<BrowserRouter><ProductsList /></BrowserRouter>);
+        await waitForElementToBeRemoved(() => screen.getByText('Wczytywanie...'));
+
+        const product = await screen.findByRole('heading', { level: 4, name: /samsung/i });
+        userEvent.click(product);
+
+        expect(window.location.pathname).toEqual('/products/1');
     });
 });
